d22ca797c3b94eebcab049ade4255dbc166a0578
Adam Smaka <adam@smaka.dev>
2022-03-18 11:07:53 +0100

    Add ResponsiveLayout class

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ResponsiveLayout extends StatelessWidget {
+  final Widget ultrawide;
+  final Widget wide;
+  final Widget narrow;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.ultrawide,
+    required this.wide,
+    required this.narrow,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return ultrawide;
+        } else if (constraints.maxWidth > 600) {
+          return wide;
+        } else {
+          return narrow;
+        }
+      }),
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({
     Key? key,
